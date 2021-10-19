e45f762a434e907fc410603ed5b7cd178bd6caf1
dulaziz <dul.aziz0789@gmail.com>
2021-10-19 09:58:14 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Skill Test Abdul Aziz</title>
+</head>
+<body>
+<!-- Banner -->
+<section class="banner">
+<div class="banner-title">
+	<div class="max-width">
+		<div class="alert">
+		<a href="#"><img src="img/danger.png" alt="name" /></a>
+		<h2><a href="#">Your broeser internet Explorer 11 is deprecated</a></h2>
+		</div>
+		<p>Modern browsers protect your device from viruses and support new technologies.<br><br>
+		That’s why we decided to stop supporting your current browser or your browser version. To access our website,<br>
+		please download one of the browsers below.</p>
+		<button>How do I update my browser?</button>
+	</div>
+	</div>
+			
+<!-- Banner Content -->
+	<div class="container">	
+		<div class="box chrome">
+		<div class="item">
+			<div class="icon"><img src="img/chrome.svg"></div>
+			<div class="title">Google Chrome	
+			</div><p>Version 40+</p>
+			<button>Download</button>
+		</div>
+		</div>
+		<div class="box firefox">
+		<div class="item">
+			<div class="icon"><img src="img/firefox-browser.svg"></div>
+			<div class="title">Mozilla Firefox	
+			</div><p>Version 40+</p>
+			<button>Download</button>
+		</div>
+		</div>
+		<div class="box edge">
+		<div class="item">
+			<div class="icon"><img src="img/edge.svg"></div>
+			<div class="title">Microsoft Edge	
+			</div><p>Version 40+</p>
+			<button>Download</button>
+		</div>
+		</div>
+		<div class="box safari">
+		<div class="item">
+			<div class="icon"><img src="img/safari.svg"></div>
+			<div class="title">Apple Safari	
+			</div><p>Version 40+</p>
+			<button>Download</button>
+		</div>
+		</div>
+		<div class="box opera">
+		<div class="item">
+			<div class="icon"><img src="img/opera.svg"></div>
+			<div class="title">Opera Browser	
+			</div><p>Version 40+</p>
+			<button>Download</button>
+		</div>
+		</div>
+	</div>
+</section>
+
+<!-- Content -->
+<section class="content">
+	<div class="cnt-alrt">
+		<a href="#"><img src="img/danger.png" alt="name" /></a>
+		<h5><a href="#">Your broeser internet Explorer 11 is deprecated</a></h5>
+		<p>Modern browsers protect your device from viruses and support new technologies.
+		<br><br>
+		That’s why we decided to stop supporting your current browser or your browser version. To access our website,<br>
+		please download one of the browsers below.</p>
+	</div>
+	<div class="cnt-chrome">
+			<div class="cnt-icn-chrm"><img src="img/chrome.svg"></div>
+			<div class="title"><h4>Google Chrome</h4>
+				<h6>Version 40+</h6>
+			</div>
+			<div><img src="img/direct-download.png"></div>
+	</div>
+	<div class="cnt-mozila">
+			<div class="cnt-icn-mzla"><img src="img/firefox-browser.svg"></div>
+			<div class="title"><h4>Mozilla Firefox</h4>
+				<h6>Version 40+</h6>
+			</div>
+			<div><img src="img/direct-download.png"></div>
+	</div>
+	<div class="cnt-edge">
+			<div class="cnt-icn-edge"><img src="img/edge.svg"></div>
+			<div class="title"><h4>Microsoft Edge</h4>
+				<h6>Version 40+</h6>
+			</div>
+			<div><img src="img/direct-download.png"></div>
+	</div>
+	<div class="cnt-safari">
+			<div class="cnt-icn-safari"><img src="img/safari.svg"></div>
+			<div class="title"><h4>Apple Safari</h4>
+				<h6>Version 40+</h6>
+			</div>
+			<div><img src="img/direct-download.png"></div>
+	</div>
+	<div class="cnt-opera">
+			<div class="cnt-icn-opera"><img src="img/opera.svg"></div>
+			<div class="title"><h4>Opera Browser</h4>
+				<h6>Version 40+</h6>
+			</div>
+			<div><img src="img/direct-download.png"></div>
+	</div>
+</section>
+</body>
+</html>
